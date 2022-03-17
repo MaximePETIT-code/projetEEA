7025dba6c685f4cbdaa8618dd02e526acdc11ff7
Maxime Petit <contactmaximepetit@gmail.com>
2022-03-17 14:49:26 +0100

    login

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,38 @@
-let verouillage = document.getElementById("verouillage");
+const verouillage = document.getElementById("verouillage");
+const date = document.getElementById("date-info");
+const userInfo = document.getElementById("user");
+const passwordInput = document.getElementById("password");
+const goodPassword = 'bobby';
 
-verouillage.addEventListener("click", ()=> {
+verouillage.addEventListener("click", unlock)
+
+function unlock(){
     verouillage.classList.remove("lock");
     verouillage.classList.add("unlock");
+    date.classList.add("disable");
+    userInfo.classList.remove("disable");
+}
+
+passwordInput.addEventListener("keyup", (e) =>{
+    if (e.keyCode === 13) {
+        let password = passwordInput.value;
+        passwordVerify(password);
+    }
+    
 })
+
+function passwordVerify(password){
+    if(password == goodPassword){
+        console.log("bon mot de passe");
+    }else{
+        console.log("mauvais mot de passe");
+        passwordInput.value = '';
+    }
+}
+
+
+
+
+
+// mot de passe
+
