3755eddd7e22d796df439a2c189af092a7bd3ae6
Maxime Petit <contactmaximepetit@gmail.com>
2022-03-25 11:01:55 +0100

    date

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,41 @@
 const verouillage = document.getElementById("verouillage");
 const bureau = document.getElementById("bureau");
-const date = document.getElementById("date-info");
+const dateHtml = document.getElementById("date-info");
 const userInfo = document.getElementById("user");
 const passwordInput = document.getElementById("password");
+const numericDate = document.querySelector('.numericDate');
+const heureActuelHTML = document.querySelectorAll('.heureActuel');
+const dateVerouillageHTML = document.getElementById('dateVerouillage');
 
 const goodPassword = "bobby";
 
+console.log(dateVerouillageHTML);
+
+const date = new Date();
+const options = { year: "numeric", month: "numeric", day: "numeric"};
+const options2 = {weekday: 'long', month: "long", day: "numeric"};
+let dateActuel = date.toLocaleDateString("fr-FR", options);
+let dateActuelVerouillage = date.toLocaleDateString("fr-FR", options2);
+
+dateVerouillageHTML.innerHTML = dateActuelVerouillage;
+
+numericDate.innerHTML = dateActuel;
+
+function affichZero(nombre) {
+
+    return nombre < 10 ? '0' + nombre : nombre;
+    }
+    function heure() {
+     
+        const infos = new Date();
+
+        for (i = 0; i < heureActuelHTML.length; i++) {
+            heureActuelHTML[i].innerHTML = ' ' + affichZero(infos.getHours()) + ':' + affichZero(infos.getMinutes());
+        }
+    }
+    window.onload = function() {
+    setInterval("heure()", 100);
+    };
 
 verouillage.addEventListener("click", unlock);
 
@@ -13,7 +43,7 @@ verouillage.addEventListener("click", unlock);
 function unlock(){
     verouillage.classList.remove("lock");
     verouillage.classList.add("unlock");
-    date.classList.add("disable");
+    dateHtml.classList.add("disable");
     userInfo.classList.remove("disable");
 }
 
